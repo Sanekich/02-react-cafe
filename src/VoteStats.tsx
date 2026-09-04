166e16ae1f3f0e1a2d4600729d0e17a1f4ff8898
Sanekich <alex.d.muratov@gmail.com>
2026-09-04 14:44:26 +0200

    ffff

---
+++ src/VoteStats.tsx
@@ -0,0 +1,29 @@
+import styles from './VoteStats.module.css';
+
+interface VoteTypes {
+    good: number;
+    neutral: number;
+    bad: number;
+}
+
+interface voteStats{
+    votes: VoteTypes;
+    totalVotes: number;
+    positiveRate: number;
+}
+
+function VoteStats({votes,totalVotes,positiveRate}: voteStats){
+    return(
+        <>
+            <div className={styles.container}>
+                <p className={styles.stat}>Good: <strong>{votes.good}</strong></p>
+                <p className={styles.stat}>Neutral: <strong>{votes.neutral}</strong></p>
+                <p className={styles.stat}>Bad: <strong>{votes.bad}</strong></p>
+                <p className={styles.stat}>Total: <strong>{totalVotes}</strong></p>
+                <p className={styles.stat}>Positive: <strong>{positiveRate}%</strong></p>
+            </div>
+        </>
+    )
+}
+
+export default VoteStats;
